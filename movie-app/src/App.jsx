4aa8ae8dbf6b15d7abb39da6656c64ba00c1b0fa
Thanya <cheveztj@gmail.com>
2025-08-16 19:52:53 -0400

    first git commit push

---
+++ movie-app/src/App.jsx
@@ -0,0 +1,27 @@
+import { useState } from "react";
+
+const Card = ({ title }) => {
+    const [hasLiked, setHasLiked] = useState(false);
+
+    return (
+        <div className="card">
+        <h2>{title}</h2>
+
+            <button onClick={() => setHasLiked(true)}>
+                {hasLiked ? 'Liked' : 'Like'}
+            </button>
+        </div>
+    )
+}
+const App = () => {
+
+    return (
+        <div className="card-container">
+            <Card title="Star Wars" rating={5} isCool={true} />
+            <Card title="Avatar"/>
+            <Card title="The Lion King"/>
+        </div>
+    )
+}
+
+export default App
